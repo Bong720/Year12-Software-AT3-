d060d0bc05c728725d29cf66f0e744a299e2953f
Bong720 <eric.bang4@education.nsw.gov.au>
2026-05-12 00:58:53 +0000

    added pandas and made phase 1 code work

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+
+
+
 print("--- Loading and Cleaning Data ---")
 
 # TODO 1: Load the raw dataset into a variable called df_raw (Hint: use encoding='latin-1' to avoid Unicode errors)
